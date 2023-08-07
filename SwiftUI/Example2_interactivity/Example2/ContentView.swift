b359d295ff7419a0fd509fbb99c66774b1767fa3
mgaiduk <mgaiduk@sharechat.co>
2023-08-07 12:09:05 +0100

    swiftui onappear bug

--- SwiftUI/Example2_interactivity/Example2/ContentView.swift
+++ SwiftUI/Example2_interactivity/Example2/ContentView.swift
@@ -1,35 +1,77 @@
 //
 //  ContentView.swift
-//  Example2
+//  MyAudioPlayerApp
 //
-//  Created by Maksim Gaiduk on 23/02/2023.
+//  Created by test on 4/18/23.
 //
 
 import SwiftUI
 
-struct ContentView : View {
+let defaultDeck = Deck()
+
+class Deck: ObservableObject {
+    @Published var currentCard: String? = nil
+    
+    func addCard() {
+        self.currentCard = "NewCard"
+    }
+    
+    func deleteCard() {
+        currentCard = nil
+    }
+}
+
+struct EditCardView: View {
+    @ObservedObject var deck: Deck
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        Form {
-            Section {
-                VStack {
-                    Text("TextInput")
-                        .overlay(alignment: .topTrailing) {
-                            Text("SuggestOptions")
-                                .frame(minHeight: 400)
-                                .background(Color.green)
-                                .offset(x: 25, y: 25)
-                                .opacity(0.5)
-                        }
-                }
-                .frame(minHeight: 400, alignment: .topLeading)
+        NavigationView {
+            Button("Delete") {
+                // Perform save action here
+                // hack to update the parent view
+                deck.deleteCard()
+                presentationMode.wrappedValue.dismiss()
             }
         }
     }
 }
 
 
+
+struct ContentView: View {
+    @StateObject var deck: Deck
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Hi!")
+                if let currentCard = deck.currentCard {
+                    Text("CurrentCard: \(deck.currentCard!)")
+                        .onAppear {
+                            print("OnAppear called, currentCard: \(currentCard)", Date())
+                        }
+                        .onDisappear {
+                            print("onDisappear called, currentCard: \(currentCard)", Date())
+                        }
+                } else {
+                    Text("Out of cards!")
+                }
+                Button("Add") {
+                    deck.addCard()
+                }
+                if let currentCard = deck.currentCard {
+                    NavigationLink {
+                        EditCardView(deck: deck)
+                    } label: {
+                        Text("edit")
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(deck: defaultDeck)
     }
 }
